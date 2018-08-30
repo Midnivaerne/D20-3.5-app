ec8a9bc734f38aecf2bfdc2ff11889d71c43946b
K <krzygol16@gmail.com>
2018-08-30 11:04:40 +0200

    Base UI implementation

--- app/src/main/java/com/aurora/d20_35_app/activities/MainActivity.java
+++ app/src/main/java/com/aurora/d20_35_app/activities/MainActivity.java
@@ -1,5 +1,8 @@
-package com.aurora.d20_35_app;
+package com.aurora.d20_35_app.activities;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +11,11 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+
+import com.aurora.d20_35_app.R;
+import com.aurora.d20_35_app.fragments.Fragment_DM;
+import com.aurora.d20_35_app.fragments.Fragment_PC;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +23,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        Button DM_button = findViewById(R.id.DMbutton);
+        DM_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment_DM fragment_DM = new Fragment_DM();
+                loadFragment(fragment_DM, "content_DM");
+            }
+        });
+
+        Button PC_button = findViewById(R.id.PCbutton);
+        PC_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment_PC fragment_PC = new Fragment_PC();
+                loadFragment(fragment_PC, "content_PC");
+            }
+        });
+
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -27,6 +53,24 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    public void loadFragment(Fragment frag, String tag) {
+
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+
+        Fragment fragment = getFragmentManager().findFragmentById(R.id.fragmentContainer);
+        if (fragment == null) {
+            ft.add(R.id.fragmentContainer, frag, tag);
+        } else {
+            ft.replace(R.id.fragmentContainer, frag, tag);
+        }
+        ft.addToBackStack(null);
+
+        ft.commit();
+
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
